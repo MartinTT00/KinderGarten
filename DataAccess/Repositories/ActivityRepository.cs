eeb96a071b040ac906b0325fc6f67144376272e3
MartinTT00 <61382773+MartinTT00@users.noreply.github.com>
2020-05-13 11:28:24 +0300

    +2 more Repositories

---
+++ DataAccess/Repositories/ActivityRepository.cs
@@ -0,0 +1,76 @@
+﻿using DataStructure;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+
+namespace DataAccess.Repositories
+{
+    public class ActivityRepository
+    {
+        private readonly KinderGartenDBContext kinderGartenDBContext = new KinderGartenDBContext();
+
+        public ActivityRepository(KinderGartenDBContext kinderGartenDBContext)
+        {
+            this.kinderGartenDBContext = kinderGartenDBContext;
+        }
+        public void Create()
+        {
+
+        }
+
+
+        public void Create(Activity activity)
+        {
+            kinderGartenDBContext.Activities.Add(activity);
+        }
+
+        public List<Activity> Read()
+        {
+            List<Activity> allActivities = kinderGartenDBContext.Activities.ToList();
+            return allActivities;
+        }
+
+        public Activity Update(int id)
+        {
+            Activity activity = new Activity();
+            activity = GetByID(id);
+            return activity;
+        }
+
+        public void Update(Activity activity)
+        {
+            kinderGartenDBContext.Entry(activity).State = EntityState.Modified;
+        }
+        public Activity Delete(int? id)
+        {
+            Activity activity = new Activity();
+            activity = GetByID(id);
+            return activity;
+        }
+        public void Delete(int id)
+        {
+            Activity activity = new Activity();
+            activity = GetByID(id); kinderGartenDBContext.Activities.Remove(activity);
+        }
+
+        public List<Activity> GetAll()
+        {
+            List<Activity> allActivities = kinderGartenDBContext.Activities.ToList();
+            return allActivities;
+        }
+        public Activity GetByID(int id)
+        {
+            Activity activity = new Activity();
+            activity = kinderGartenDBContext.Activities.Find(id);
+            return activity;
+        }
+        public Activity GetByID(int? id)
+        {
+            Activity activity = new Activity();
+            activity = kinderGartenDBContext.Activities.Find(id);
+            return activity;
+        }
+
+    }
+}
---
+++ DataAccess/Repositories/LocationRepository.cs
@@ -0,0 +1,75 @@
+﻿using DataStructure;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+
+namespace DataAccess.Repositories
+{
+    public class LocationRepository
+    {
+        private readonly KinderGartenDBContext kinderGartenDBContext = new KinderGartenDBContext();
+
+        public LocationRepository(KinderGartenDBContext kinderGartenDBContext)
+        {
+            this.kinderGartenDBContext = kinderGartenDBContext;
+        }
+        public void Create()
+        {
+
+        }
+
+        public void Create(Location location)
+        {
+            kinderGartenDBContext.Locations.Add(location);
+        }
+
+        public List<Location> Read()
+        {
+            List<Location> allLocations = kinderGartenDBContext.Locations.ToList();
+            return allLocations;
+        }
+
+        public Location Update(int id)
+        {
+            Location location = new Location();
+            location = GetByID(id);
+            return location;
+        }
+
+        public void Update(Location location)
+        {
+            kinderGartenDBContext.Entry(location).State = EntityState.Modified;
+        }
+        public Location Delete(int? id)
+        {
+            Location location = new Location();
+            location = GetByID(id);
+            return location;
+        }
+        public void Delete(int id)
+        {
+            Location location = new Location();
+            location = GetByID(id); kinderGartenDBContext.Locations.Remove(location);
+        }
+
+        public List<Location> GetAll()
+        {
+            List<Location> allLocations = kinderGartenDBContext.Locations.ToList();
+            return allLocations;
+        }
+        public Location GetByID(int id)
+        {
+            Location location = new Location();
+            location = kinderGartenDBContext.Locations.Find(id);
+            return location;
+        }
+        public Location GetByID(int? id)
+        {
+            Location location = new Location();
+            location = kinderGartenDBContext.Locations.Find(id);
+            return location;
+        }
+
+    }
+}
